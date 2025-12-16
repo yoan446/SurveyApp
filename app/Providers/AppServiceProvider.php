998d78190881a097ea3a0e5d7e1a6d811bcd251d
yoan446 <azebazebrayan@gmail.com>
2025-12-16 05:22:27 +0100

    version 27

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       // Forcer HTTPS en production
+       if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
